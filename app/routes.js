49ccffb75fd73e07cee6689037ab933619a986cd
Sorin Vasiliu <soringabrielvasiliu@reea.net>
2015-10-02 14:42:31 +0300

    Small fixes

--- app/routes.js
+++ app/routes.js
@@ -33,10 +33,6 @@ MongoClient.connect("mongodb://localhost:27017/weather", function(err, database)
     db = database;
 });
 
-function daysOfWeek(dayIndex) {
-    return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dayIndex];
-}
-
 // expose the routes to our app with module.exports
 module.exports = function(app) {
 
@@ -156,22 +152,7 @@ app.get('/api/homepage', function(req, res) {
   }
 });
 
-app.get('/api/weather', function (req, res) {
-    request('https://api.worldweatheronline.com/free/v2/weather.ashx?q=Iasi&num_of_days=5&tp=24&format=json&key=aa15be85bf9357ca90f879405497d', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var result = JSON.parse(body);
-            if (result.data && typeof result.data.error === 'undefined') {
-                result.data.weather.forEach(function(day) {
-                    day.dayOfWeek = daysOfWeek(new Date(day.date).getDay());
-                });
-            }
-            res.send(result.data);
-        } else {
-            console.log(error, response.statusCode, body);
-        }
-        res.end("");
-    });
-});
+app.get('/api/weather',function(req,res){request('https://api.worldweatheronline.com/free/v2/weather.ashx?q=Iasi&num_of_days=5&tp=24&format=json&key=aa15be85bf9357ca90f879405497d',function(error,response,body){res.send(body)})});
 
 app.post('/api/recovery', function(req, res) {
   var recoveryErrorCode = 0;
--- public/js/services/Weather.js
+++ public/js/services/Weather.js
@@ -2,10 +2,10 @@ angular.module('UsersLibrary')
 
 	// super simple service
 	// each function returns a promise object 
-	.factory('Weather', ['$http', function($http) {
+	.factory('Weather', ['$http', '$routeParams', function($http, $routeParams) {
 		return {
-			get : function() {
-				return $http.get('/api/weather');
+			get:function(){
+				return $http.get('/api/weather')
 			}
 		}
 	}]);
